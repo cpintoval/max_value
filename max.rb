47ea58f76e0090d0a5bb29422820126b62b2a7d1
cpintoval <cpintovalderrama@gmail.com>
2015-08-25 15:43:49 +0000

    Added max value functionality

---
+++ max.rb
@@ -0,0 +1,22 @@
+# Find the maximum 
+def maximum(arr)
+  result = arr.empty? ? nil : 0
+  if(result != nil)
+    arr.each {|x| result = x > result ? x : result}
+  end
+  return result
+end
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
